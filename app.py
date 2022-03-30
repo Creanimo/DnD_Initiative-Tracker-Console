6fdc250651e525d30a105d6031ddeb5b0a0d1714
Creanimo <ferdinand@creanimo.net>
2022-03-30 23:42:44 +0200

    basic combat initiative tracker

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
+import operator
+
 class uitools:
-   def question_yn(question) :
-    yes = {'yes','y', 'ye', ''}
-    no = {'no','n'}
-    print(question)
-    choice = input().lower()
-    if choice in yes:
-        return True
-    elif choice in no:
-        return False
-    else:
-        print("Please respond with 'yes' or 'no'")
-        question_yn(question)
+    def question_yn(question) :
+        yes = {'yes','y', 'ye', ''}
+        no = {'no','n'}
+        while True:
+            print(question)
+            try:
+                givenanswer = input().lower()
+                if givenanswer in yes:
+                    return True
+                elif givenanswer in no:
+                    return False
+                else:
+                    raise Exception("Please respond with 'yes' or 'no'.")
+            except Exception as e:
+                print(e)
+            continue
+
+    def question_int(question) :
+        while True:
+            print(question)
+            try:
+                givenanswer = int(input())
+                return givenanswer
+            except:
+                print("Please enter a whole number.")
+            continue
+
 
 class character:
     def __init__(self, name, initiative, armorClass) :
         self.name = name
         self.initiative = initiative
         self.armorClass = armorClass
+
 class encounter:
     def __init__(self, name, participants) :
         self.name = name
@@ -26,7 +44,30 @@ class encounter:
             print("Participants has to be a list")
 
     def addCharacter(self, addMe) :
+        # check if name already exist
+        for i in self.participants:
+            if i.name == addMe:
+                duplicateAppendix = "*"
+                addMe = " ".join((addMe, duplicateAppendix))
         self.participants.append(character(addMe,0,0))
+        print(f"Added {addMe}")
+
+    def battleCommands():
+        commandNext = {"n","next",""}
+        commandExit = {"e", "exit", "q", "quit"}
+        while True:
+            print("What now?")
+            battleAction = input().lower()
+            if battleAction in commandNext:
+                return
+            elif battleAction in commandExit:
+                return "exitEncounter"
+            else:
+                print("Make a valid choice! n = next, e = exit")
+        
+
+
+# Build character list for the encounter
 
 choice_defaultCharacters = uitools.question_yn("Would you like to load the default hero characters?")
 if choice_defaultCharacters == True :
@@ -50,3 +91,30 @@ while True :
     for i in CurrentEncounter.participants:
       print(i.name)
     break
+
+# Getting the initiative order
+
+print("Time to roll initiative")
+for i in CurrentEncounter.participants:
+    i.initiative = uitools.question_int(f"Initiative for {i.name}?")
+print("Initiative is as follows:")
+sortedInitiative = sorted(CurrentEncounter.participants, key=lambda x: x.initiative, reverse = True)
+for i in sortedInitiative:
+    print(f"{i.name} - {i.initiative}")
+
+# Combat
+
+print("Starting Combat")
+round = 0
+while True:
+    round+= 1
+    print(f"Round {round}")
+    for i in sortedInitiative:
+        ExitAction = False
+        print(f"Your turn, {i.name}")
+        collectCommand = ""
+        collectCommand = encounter.battleCommands()
+        if collectCommand ==  "exitEncounter":
+            break
+    if collectCommand ==  "exitEncounter":
+        break
